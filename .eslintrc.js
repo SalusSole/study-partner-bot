20f8429d8aabddf4fd514b11eb4a5ad4659b883b
Salvador Solis <celularfireweb@gmail.com>
2022-09-22 10:11:03 -0500

    Fisrt commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always",
+            { "omitLastInOneLineBlock": true}
+        ],
+        "no-multi-spaces": ["error"],
+        "prefer-const": ["error", {
+            "destructuring": "all"
+        }],
+        "comma-dangle": ["error", "never"],
+        "array-callback-return": "error"
+    }
+};
